6332d2c6fae38115737525b62f2801c02dcc1c8f
idperezSk <idperez@skanhawk.com>
2022-09-01 13:16:46 -0500

    se agregan las rutas de los rigs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.users import user
-
+from routes.rigs import rigs
 app = FastAPI()
 
 app.include_router(user) # declaro que la app usará las rutas definidas para user
+app.include_router(rigs)
