d315f9dc1795e575bba300b548cff96a44efcab4
idperezSk <idperez@skanhawk.com>
2022-09-02 11:10:45 -0500

    cambio configuración base de datos

--- config/db.py
+++ config/db.py
@@ -7,7 +7,7 @@ DB_USER = 'root'
 DB_PASSWORD = 'skanhawkpassword'
 DB_HOST= 'localhost'
 
-engine = create_engine(f'postgresql+pygresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DATABASE}')
+engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DATABASE}')
 
 meta = MetaData()
 
