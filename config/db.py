280a0b15e863aeea236ab8dc788aaa64e848e582
idperezSk <idperez@skanhawk.com>
2022-09-02 11:03:25 -0500

    cambios de ejecuciony conexiónn a base de datos

--- config/db.py
+++ config/db.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine, MetaData
 import os
 
-PORT = os.environ['PORT']
-DATABASE = os.environ['DATABASE']
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_HOST= os.environ['DB_HOST']
-
-
-
+PORT = 3306
+DATABASE = 'skh_db'
+DB_USER = 'root'
+DB_PASSWORD = 'skanhawkpassword'
+DB_HOST= 'localhost'
 
 engine = create_engine(f'postgresql+pygresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DATABASE}')
 
