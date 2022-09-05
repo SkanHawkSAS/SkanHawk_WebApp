78dcd4d11d4112f7284c15093f57ffdd0cc8eb05
idperezSk <idperez@skanhawk.com>
2022-09-05 10:10:47 -0500

    cs

--- middlewares/verifyTokenRoute.py
+++ middlewares/verifyTokenRoute.py
@@ -3,19 +3,34 @@ from helpers.helpers import ValidateToken
 from fastapi.routing import APIRoute
 
 
+# class VerifyTokenRoute(APIRoute):
+
+#     def GetRouteHandler(self):
+#         original_route = super().GetRouteHandler()
+
+#         async def VerifyTokenMiddleware(request:Request):
+#             token = request.headers["Authorization"].split(" ")[1]
+
+#             validationResponse = ValidateToken(token, output=False)
+#             print(validationResponse)
+#             if validationResponse == None:
+#                 return await original_route(request)
+#             else:
+#                 return validationResponse
+            
+#         return VerifyTokenMiddleware
+
 class VerifyTokenRoute(APIRoute):
-
-    def GetRouteHandler(self):
-        original_route = super().GetRouteHandler()
-
-        async def VerifyTokenMiddleware(request:Request):
+    def get_route_handler(self):
+        original_route = super().get_route_handler()
+        
+        async def verify_token_middleware(request:Request):
             token = request.headers["Authorization"].split(" ")[1]
-
-            validationResponse = ValidateToken(token, output=False)
-            print(validationResponse)
-            if validationResponse == None:
+            
+            validation_response = ValidateToken(token, output=False)
+            if validation_response == None:
                 return await original_route(request)
             else:
-                return validationResponse
-            
-        return VerifyTokenMiddleware
+                return validation_response
+
+        return verify_token_middleware
