f894b643126ea71e50c55d876487f786e5a40b4d
idperezSk <idperez@skanhawk.com>
2022-09-05 10:20:41 -0500

    Proteccion de rutas con JWT

--- middlewares/verifyTokenRoute.py
+++ middlewares/verifyTokenRoute.py
@@ -3,34 +3,17 @@ from helpers.helpers import ValidateToken
 from fastapi.routing import APIRoute
 
 
-# class VerifyTokenRoute(APIRoute):
-
-#     def GetRouteHandler(self):
-#         original_route = super().GetRouteHandler()
-
-#         async def VerifyTokenMiddleware(request:Request):
-#             token = request.headers["Authorization"].split(" ")[1]
-
-#             validationResponse = ValidateToken(token, output=False)
-#             print(validationResponse)
-#             if validationResponse == None:
-#                 return await original_route(request)
-#             else:
-#                 return validationResponse
-            
-#         return VerifyTokenMiddleware
-
 class VerifyTokenRoute(APIRoute):
-    def get_route_handler(self):
-        original_route = super().get_route_handler()
+    def GetRouteHandler(self):
+        original_route = super().GetRouteHandler()
         
-        async def verify_token_middleware(request:Request):
+        async def VerifyTokenMiddleware(request:Request):
             token = request.headers["Authorization"].split(" ")[1]
             
-            validation_response = ValidateToken(token, output=False)
-            if validation_response == None:
+            validationResponse = ValidateToken(token, output=False)
+            if validationResponse == None:
                 return await original_route(request)
             else:
-                return validation_response
+                return validationResponse
 
-        return verify_token_middleware
+        return VerifyTokenMiddleware
