529488e4ce46db34a751b1f7c1dd8f755314f1c5
idperezSk <idperez@skanhawk.com>
2022-09-05 09:04:39 -0500

    se agrego el router de la autenticacion

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routes.users import user
 from routes.rigs import rig
+from routes.auth import auth
 app = FastAPI()
 
 app.include_router(user) # declaro que la app usará las rutas definidas para user
 app.include_router(rig)
+app.include_router(auth)
