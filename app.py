3b1f08caaf6e43fb175d756895037383e21184fb
idperezSk <idperez@skanhawk.com>
2022-09-15 08:40:00 -0500

    sd

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://192.168.1.71/4200",
-    "http://localhost",
-    "http://localhost:8080",
+    "http://192.168.1.71:4200"
 ]
 
 app.add_middleware(
